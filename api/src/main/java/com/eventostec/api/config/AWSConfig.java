f9e4ce026a14bc69cb3ee21ef23362e636e9fa4d
analarap <analarapupo@gmail.com>
2025-01-13 12:11:18 -0300

    fix: adding method to search credentials locally

--- api/src/main/java/com/eventostec/api/config/AWSConfig.java
+++ api/src/main/java/com/eventostec/api/config/AWSConfig.java
@@ -1,5 +1,6 @@
 package com.eventostec.api.config;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +17,7 @@ public class AWSConfig {
     public AmazonS3 createS3Instance(){
         return AmazonS3ClientBuilder
                 .standard() // Utilizará as credenciais Standard que estiverem configuradas na máquina
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .withRegion(awsRegion)
                 .build();
     }
