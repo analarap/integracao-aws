e85bb66f8b813bfb59844a1a8eff9f75ea46eadf
analarap <analarapupo@gmail.com>
2025-01-14 09:37:24 -0300

    remove: comments removed

--- api/src/main/java/com/eventostec/api/dto/event/EventRequestDto.java
+++ api/src/main/java/com/eventostec/api/dto/event/EventRequestDto.java
@@ -2,8 +2,6 @@ package com.eventostec.api.dto.event;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Date;
 
-//
 public record EventRequestDto(String title, String description, Long date, String city, String state, Boolean remote, String eventUrl, MultipartFile image) {
 }
